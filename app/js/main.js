374dcf6765b2c45b6ff227ba88e442fd39d1865d
Joel Herron <herronj@uww.edu>
2014-04-01 09:59:48 -0500

    added jshint ignore for jquery $

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
+/*global $:false */
 $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') || location.hostname === this.hostname) {        var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
